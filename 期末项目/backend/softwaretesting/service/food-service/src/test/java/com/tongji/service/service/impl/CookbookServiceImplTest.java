48a36790234fde6f3ef0cb5f9c048798bfc99569
jixiangyuyu <2393063097@qq.com>
2024-06-09 23:44:15 +0800

    食谱模块测试

---
+++ 期末项目/backend/softwaretesting/service/food-service/src/test/java/com/tongji/service/service/impl/CookbookServiceImplTest.java
@@ -0,0 +1,64 @@
+package com.tongji.service.service.impl;
+
+import com.tongji.model.dto.CookBookReturnDTO;
+import com.tongji.model.dto.CookbookDTO;
+import com.tongji.model.vo.ResponseResult;
+import com.tongji.service.service.ICookbookService;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.util.AssertionErrors;
+
+
+
+@Slf4j
+@SpringBootTest
+class CookbookServiceImplTest {
+
+    @Autowired
+    private ICookbookService cookbookService;
+
+    @BeforeEach
+    void setUp() {
+        log.info("开始测试");
+    }
+
+    @AfterEach
+    void tearDown() {
+        log.info("测试结束");
+    }
+
+    @Test
+    @DisplayName("测试数据不存在")
+    void testNotExitsGetCookbook() {
+        log.info("测试数据库无结果");
+        CookbookDTO cookbookDTO = new CookbookDTO();
+
+        cookbookDTO.setLocation("广西");
+        cookbookDTO.setSeason("春天");
+        cookbookDTO.setNumber(-1);
+
+        ResponseResult result = this.cookbookService.getCookbook(cookbookDTO);
+        CookBookReturnDTO cookBookReturnDTO = (CookBookReturnDTO)result.getData();
+        AssertionErrors.assertNull("应该获取不到数据", cookBookReturnDTO);
+    }
+
+    @Test
+    @DisplayName("测试数据存在")
+    void testExitsGetCookbook() {
+        log.info("测试数据库有结果");
+        CookbookDTO cookbookDTO = new CookbookDTO();
+
+        cookbookDTO.setLocation("东北");
+        cookbookDTO.setSeason("春季");
+        cookbookDTO.setNumber(1);
+
+        ResponseResult result = this.cookbookService.getCookbook(cookbookDTO);
+        CookBookReturnDTO cookBookReturnDTO = (CookBookReturnDTO)result.getData();
+        AssertionErrors.assertNotNull("测试数据应该存在", cookBookReturnDTO);
+    }
+}
