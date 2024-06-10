337d95c641da76976edd05e4ad10e073f226e1fd
PeachOolongTea <805532481@qq.com>
2024-06-10 23:57:09 +0800

    create frontend files

--- 平时作业/代码/frontend/src/router/index.ts
+++ 平时作业/代码/frontend/src/router/index.ts
@@ -3,18 +3,20 @@ import HomeView from '../views/HomeView.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
-    path: '/',
+    path: '/home',
     name: 'home',
     component: HomeView
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+    path:"",
+    redirect: "/home"
+ },
+  {
+    //任意匹配
+    path:"/:pathMatch(.*)",
+    name:"notfound",
+    component: () => import( /* webpackChunkName: 'NotFound' */ "../views/NotFound.vue"),
+   },
 ]
 
 const router = createRouter({
